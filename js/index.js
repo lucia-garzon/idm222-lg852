68b96d8b4062294c5e4ababa4fbf83f67e67416f
lucia-garzon <lucgarzon.uni@gmail.com>
2024-03-14 15:52:34 -0400

    remove comments

--- js/index.js
+++ js/index.js
@@ -33,29 +33,25 @@ function rearrangeElementsForMobile() {
     const images = proj2FinalImages.querySelectorAll('img.rounded-image');
     const texts = proj2FinalImages.querySelectorAll('.proj2-final-text');
 
-    // Clear current contents
     proj2FinalImages.innerHTML = '';
 
-    // Assuming the order is Desktop, Tablet, Mobile based on your description
+    
     for (let i = 0; i < images.length; i++) {
-        proj2FinalImages.appendChild(images[i]); // Append image
-        proj2FinalImages.appendChild(texts[i]); // Append corresponding text
+        proj2FinalImages.appendChild(images[i]); 
+        proj2FinalImages.appendChild(texts[i]); 
     }
 }
 
 function checkAndRearrange() {
-    // Check if screen width is less than or equal to 767px
+    
     if (window.innerWidth <= 767) {
         rearrangeElementsForMobile();
     } else {
-        // You may need to add logic here to rearrange elements back to the original state
-        // if the user resizes the window back to more than 767px
+    
     }
 }
 
-// Listen for window resize events
 window.addEventListener('resize', checkAndRearrange);
 
-// Initial check in case the page is loaded on a small screen
 window.addEventListener('DOMContentLoaded', checkAndRearrange);
 
