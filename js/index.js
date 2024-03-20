aea9e750d8daf907eb8a9cc7ff07fad9f93ad9d5
lucia-garzon <lucgarzon.uni@gmail.com>
2024-03-20 18:22:58 -0400

    hopefully fix burger-x

--- js/index.js
+++ js/index.js
@@ -1,22 +1,36 @@
-// @ts-check
+// const menu = document.querySelector('nav');
+
+
+
+
+
+// const menu_buttons = document.querySelectorAll('.btn_menu');
+
+// if (menu_buttons) {
+//     menu_buttons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         console.log('toggle menu');
+//         if (menu) menu.classList.toggle('open');
+//     });
+//     });
+// }
+
+
 const menu = document.querySelector('nav');
-const btn_close_menu = document.querySelector('.btn_menu_close');
-const btn_open_menu = document.querySelector('.btn_menu_open');
+const burgerX = document.querySelector('.burger-x');
+const burgerOpen = document.querySelector('.burger');
+const menuButtons = document.querySelectorAll('.btn_menu');
 
-
-
-
-const menu_buttons = document.querySelectorAll('.btn_menu');
-// console.log(menu_buttons)
-
-if (menu_buttons) {
-    menu_buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log('toggle menu');
-        if (menu) menu.classList.toggle('open');
-    });
+if (menuButtons) {
+    menuButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            console.log('toggle menu');
+            if (menu) {
+                menu.classList.toggle('open');
+                burgerX.classList.toggle('visible'); // Toggle visibility of burger-x button
+            }
+        });
     });
 }
-
 
 
